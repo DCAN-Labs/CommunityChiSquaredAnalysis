50ee20b2bdde10d7fb0e1a41014807ab7605176d
Eric Feczko <feczko@ohsu.edu>
2019-08-07 14:17:39 -0700

    compiled core enrichment algorithm for chisquared analysis.

---
+++ compile_enrichment.m
@@ -0,0 +1,3 @@
+%compiling surf code
+addpath(genpath('/mnt/max/shared/projects/FAIR_users/Feczko/code_in_dev/CommunityChisquaredAnalysis'))
+mcc -v -m -d /mnt/max/shared/projects/FAIR_users/Feczko/code_in_dev/CommunityChisquaredAnalysis -o CountSignificantEffectsByModules /mnt/max/shared/projects/FAIR_users/Feczko/code_in_dev/CommunityChisquaredAnalysis/CountSignificantEffectsByModules.m
